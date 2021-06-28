b5c10b57395304af935b1bc8397c368976179338
TanYa-Go <tanja.godinic@gmail.com>
2021-06-28 18:11:56 +0000

    Add smooth scroll to website

--- static/js/script.js
+++ static/js/script.js
@@ -25,3 +25,18 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
   });
+
+
+// smooth scroll
+$('.navbar a').on('click', function (e) {
+    if (this.hash !== '') {
+      e.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body')
+        .animate({
+          scrollTop: $(hash).offset().top
+        },800);
+    }
+  });
