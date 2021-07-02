f4c8ff08968487644a558763bf5488ac5f409d3d
TanYa-Go <tanja.godinic@gmail.com>
2021-07-02 07:15:32 +0000

    Break down the base.html to index, projects and register template pages and add routes

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -19,16 +20,44 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_skills")
-def get_skills():
+@app.route("/skills")
+def skills():
     skills = mongo.db.skills.find()
     return render_template("skills.html", skills=skills)
 
 
-@app.route("/about")
-def about():
-    return render_template("base.html")
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
