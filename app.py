29545529c54fc2ceb5e58a3fc66ced966c410461
TanYa-Go <tanja.godinic@gmail.com>
2021-07-03 22:21:27 +0000

    Add jinja loop on skills page and make changes to home page styling

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def dashboard(username):
 def logout():
     # remove user from session cookie
     flash("You have been logged out")
-    session.pop("user")
+    session.clear()
     return redirect(url_for("login"))
 
 
