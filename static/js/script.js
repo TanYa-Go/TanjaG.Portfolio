d1a0ea2641b8ebf4662d067a01d0dd550de0ae11
TanYa-Go <tanja.godinic@gmail.com>
2021-07-19 23:13:13 +0000

    Beautify code and remove commented out code on files in static folder.

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,9 @@
-
 /* hide navbar on scroll down, show on scroll up */
 let navbar = document.getElementById('navigation')
 let scrollval = 0;
 window.addEventListener('scroll', () => {
-  if(scrollval > window.scrollY) {
-      navbar.classList.remove('hide')
+  if (scrollval > window.scrollY) {
+    navbar.classList.remove('hide')
     console.log('Scroll up')
   } else {
     navbar.classList.add('hide')
@@ -17,52 +16,37 @@ window.addEventListener('scroll', () => {
 
 
 /* borrowed code from https://tobiasahlin.com/moving-letters/#10 to animate letters on home page */
-/* Wrap every letter in a span */
 if (document.querySelector('.ml10 .letters')) {
-   var textWrapper = document.querySelector('.ml10 .letters');
-    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  var textWrapper = document.querySelector('.ml10 .letters');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml10 .letter',
-    rotateY: [-90, 0],
-    duration: 1300,
-    delay: (el, i) => 45 * i
-  }).add({
-    targets: '.ml10',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+  anime.timeline({
+      loop: true
+    })
+    .add({
+      targets: '.ml10 .letter',
+      rotateY: [-90, 0],
+      duration: 1300,
+      delay: (el, i) => 45 * i
+    }).add({
+      targets: '.ml10',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
 
 }
-// // smooth scroll
-// $('.navbar a').on('click', function (e) {
-//     if (this.hash !== '') {
-//       e.preventDefault();
-  
-//       const hash = this.hash;
-  
-//       $('html, body')
-//         .animate({
-//           scrollTop: $(hash).offset().top
-//         },800);
-//     }
-//   });
 
-
-  /* -- enable tooltips -- */
-  $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();
-  });
+/* Enable tooltips  */
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
 
 
 /* remove letter animation on touch devices */
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-
-    $('body').addClass('noanimated'); // to remove transition}
+  $('body').addClass('noanimated'); // to remove transition}
 
 }
-
